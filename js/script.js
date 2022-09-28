ad5076c65888e564608ea01b55c933ca7715c0dd
misael borges cancelier <mizaelborges2011@gmail.com>
2022-09-28 12:03:56 -0300

    atualizacões dos arquivos js

--- js/script.js
+++ js/script.js
@@ -4,36 +4,39 @@ botaoInserir.addEventListener('click', function(){
     let formulario = document.querySelector('#formulario')
 
     let corredor = dadosFormulario(formulario)
-    let corredorTr = constroiTr(corredor)
+    if((corredor.nome === '') || (corredor.cpf === '') || (corredor.nascimento === '') || (corredor.endereco === '') || (corredor.cep === '') || (corredor.numero === '') || (corredor.email === '')) {
+        window.alert('Campo em Branco')
+    } else {
+        let corredorTr = constroiTr(corredor)
 
-    let tabela = document.querySelector('#corredores')
-        
-    tabela.appendChild(corredorTr)
-    formulario.reset()
+        let tabela = document.querySelector('#corredores')
+            
+        tabela.appendChild(corredorTr)
+        formulario.reset()
+    }
 })
 
 function dadosFormulario(form) {
     let corredor = {
         nome: form.nome.value,
         cpf: form.cpf.value,
-        dataNacimento: form.nascimento.value,
+        nascimento: form.nascimento.value,
         endereco: form.endereco.value,
         cep: form.cep.value,
         numero: form.numero.value,
         email:form.email.value,
-        distancia: distancia(dist), 
-        idade: idade(dataNacimento.value)
+        distancia: form.dist.value,
     }
     return corredor
 }
 
 function constroiTr(corredor) {
     let atividadeTr = document.createElement('tr')
-    atividadeTr.classList.add('paciente')
+    atividadeTr.classList.add('corredor')
 
     atividadeTr.appendChild(criarTd(corredor.nome, 'info-nome'))
     atividadeTr.appendChild(criarTd(corredor.cpf, 'info-cpf'))
-    atividadeTr.appendChild(criarTd(corredor.idade, 'info-idade'))
+    atividadeTr.appendChild(criarTd(calcIdade(corredor.nascimento), 'info-idade'))
     atividadeTr.appendChild(criarTd(corredor.numero, 'info-numero'))
     atividadeTr.appendChild(criarTd(corredor.distancia, 'info-distancia'))
 
@@ -47,22 +50,23 @@ function criarTd(dado, classe) {
     return td
 }
 
+function calcIdade(dataNascto) {
+    var d = new Date()
+    ano_atual = d.getFullYear()
+    mes_atual = d.getMonth() + 1
+    dia_atual = d.getDate()
 
-function idade(a){
-    let data = new Date()
-    let ano = data.getFullYear()
-    let idade = ano - a
-    return idade
-}
+    niver = new Date(dataNascto)
 
-function distancia(d){
-    dis = document.querySelectorAll('checket')
-    if(dis[0].checked){
-        d='5Km'
-    } else if(dis[1].checked){
-        d='10Km'
-    } else if(dis[2].checked){
-        d='20Km'
+    ano_aniversario = niver.getFullYear()
+    mes_aniversario = niver.getMonth()
+    dia_aniversario = niver.getDate()
+
+    quantos_anos = ano_atual - ano_aniversario;
+
+    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--
     }
-    return d
+
+    return quantos_anos < 0 ? 0 : quantos_anos;
 }
